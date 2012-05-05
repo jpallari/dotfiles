f7551ea63b234b0a038e4efbc65cfc8090843b7c
Jaakko Pallari <jkpl@lepovirta.net>
2012-05-05 17:45:23 +0300

    emacs: added notmuch address command

--- emacs.d/my-notmuch.el
+++ emacs.d/my-notmuch.el
@@ -5,7 +5,6 @@
   (setq user-mail-address (notmuch-user-primary-email)
         user-full-name (notmuch-user-name))
   (setq notmuch-message-headers '("Subject" "To" "From" "Cc" "Date"))
-
   (setq notmuch-search-oldest-first nil)
   (setq notmuch-saved-searches
         '(("inbox" . "tag:inbox AND tag:unread")
@@ -17,4 +16,8 @@
           ("facebook" . "tag:facebook AND tag:unread")))
 
   (define-key notmuch-search-mode-map "j" 'notmuch-search-next-thread)
-  (define-key notmuch-search-mode-map "k" 'notmuch-search-previous-thread))
+  (define-key notmuch-search-mode-map "k" 'notmuch-search-previous-thread)
+
+  (when (require 'notmuch-address nil t)
+    (setq notmuch-address-command (concat (getenv "HOME") "/bin/nottoomuch-addresses.sh"))
+    (notmuch-address-message-insinuate)))

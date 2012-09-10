275294b045d4cca8f7b304583a7c7bd4ec5d4d3b
Jaakko Pallari <jkpl@lepovirta.org>
2012-09-10 11:03:02 +0300

    Moved stuff, added packages, added SLIME

---
+++ emacs.d/slime.el
@@ -0,0 +1,8 @@
+;;;; slime.el -- SLIME related settings
+
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
+(setq slime-protocol-version 'ignore
+      inferior-lisp-program "sbcl"
+      slime-complete-symbol-function 'slime-fuzzy-complete-symbol
+      slime-when-complete-filename-expand t
+      slime-truncate-lines nil)

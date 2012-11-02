10f0efe0bc3f8f3de5883f3deadca7cd7839d9b2
Jaakko Pallari <jkpl@lepovirta.org>
2012-11-02 18:59:49 +0200

    Emacs: removed package.el, and added a fetch script for it

---
+++ emacs.d/fetch-package-el.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script fetches the last Emacs 23 compatible version of package.el
+
+wget http://repo.or.cz/w/emacs.git/blob_plain/1a0a666f941c99882093d7bd08ced15033bc3f0c:/lisp/emacs-lisp/package.el -O package.el

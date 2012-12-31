b51ff1aca6cc848d908fe2ad66b871a15b071ae8
Jaakko Pallari <jkpl@lepovirta.org>
2012-12-31 11:52:46 +0200

    Emacs: Widget field coloring

--- emacs.d/custom-file.el
+++ emacs.d/custom-file.el
@@ -23,4 +23,4 @@
  '(rst-level-4-face ((t (:background "grey64" :foreground "black"))) t)
  '(rst-level-5-face ((t (:background "grey57" :foreground "black"))) t)
  '(rst-level-6-face ((t (:background "grey50" :foreground "black"))) t)
- '(widget-field ((t (:background "#3a3a3a" :foreground "#d7ff5f")))))
+ '(widget-field ((((background light)) (:background "light gray" :foreground "black")) (((background dark)) (:background "#3a3a3a" :foreground "#d7ff5f")))))

09d430b0244b4129cd00735fa96f5be33b215921
Jaakko Pallari <jkpl@lepovirta.org>
2013-01-30 22:18:45 +0200

    Emacs: removed custom-file.el and modesettings.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,14 +4,11 @@
 (when (file-accessible-directory-p "~/.emacs.d/vendor")
   (let ((default-directory "~/.emacs.d/vendor"))
     (normal-top-level-add-subdirs-to-load-path)))
-(setq custom-file "~/.emacs.d/custom-file.el")
 
 ;; Variables and constants
 (defconst my-load-files
   (list "~/.emacs.d/pkg-management.el"  ; package management
         "~/.emacs.d/vendor/loaddefs.el" ; vendor
-        "~/.emacs.d/modesettings.el"    ; mode settings
-        custom-file                     ; customizations file
         "~/.emacs.local")               ; local customizations
   "List of files to load during start up.")
 
@@ -132,9 +129,11 @@ light schemes in the current frame."
         (set-default-face-fg-bg "grey10" "grey" "white smoke" "black" frame)
       (set-default-face-fg-bg "black" "white" "white" "black" frame))))
 
-(defun ido-vertical (&optional vertical)
+(defun ido-vertical (&optional arg)
+  "Switches between vertical and horizontal style of listing in
+IDO. Always switches to vertical style if ARG is non-nil."
   (interactive)
-  (if (or vertical (not (string= (substring (car ido-decorations) 0 1) "\n")))
+  (if (or arg (not (string= (substring (car ido-decorations) 0 1) "\n")))
       (progn ;; Set vertical
         (setq ido-decorations ido-decorations-vertical)
         (add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation))
@@ -242,23 +241,128 @@ light schemes in the current frame."
 (put 'set-goal-column 'disabled nil)    ; Enable set goal column
 (put 'narrow-to-region 'disabled nil)   ; Enable narrow to region
 
+;; Automode
+(add-to-list 'auto-mode-alist '("\\.emacs.local$" . emacs-lisp-mode))
+
+;; Mode settings
+(defun ms-mail ()
+  "Email hook function."
+  (turn-on-auto-fill)
+  (setq fill-column 72))
+
+(defun ms-org ()
+  "Org mode hook function."
+  (setq org-hide-leading-stars t))
+
+(defun ms-comint ()
+  "Comint mode hook function."
+  (setq  comint-completion-addsuffix t
+         comint-completion-autolist t
+         comint-input-ignoredups t
+         comint-scroll-show-maximum-output t
+         comint-scroll-to-bottom-on-input t
+         comint-scroll-to-bottom-on-output t))
+
+(defun ms-rst ()
+  "RST hook function."
+  (turn-on-auto-fill)
+  (setq tab-width 4
+        c-basic-offset 4
+        fill-column 79
+        whitespace-line-column 79))
+
+(defun ms-python ()
+  "Python hook function."
+  (defun pep8 (&optional buffer)
+    (interactive "bPEP8 check buffer: ")
+    (python-check
+     (concat "pep8 "
+             (buffer-file-name (get-buffer buffer)))))
+  (defun pyflakes (&optional buffer)
+    (interactive "bPyFlakes check buffer: ")
+    (python-check
+     (concat "pyflakes "
+             (buffer-file-name (get-buffer buffer)))))
+  (turn-on-auto-fill)
+  (whitespace-mode 1)
+  (local-set-key (kbd "RET") 'newline)
+  (local-set-key (kbd "C-c C-q") 'jedi)
+  (setq tab-width 4
+        c-basic-offset 4
+        py-indent-offset 4
+        python-indent-offset 4
+        whitespace-line-column 79
+        fill-column 79))
+
+(defun ms-ibuffer ()
+  "IBuffer hook function"
+  (ibuffer-auto-mode 1)
+  (ibuffer-switch-to-saved-filter-groups "default"))
+
+(defun ms-eshell ()
+  "EShell hook function"
+  (setq eshell-prompt-function (lambda () "$ "))
+  (defun eshell/clear ()
+    "Clear buffer"
+    (interactive)
+    (let ((inhibit-read-only t)) (erase-buffer))))
+
 ;; Hooks
+(add-hook 'rst-mode-hook 'ms-rst)
+(add-hook 'python-mode-hook 'ms-python)
+(add-hook 'org-mode-hook 'ms-org)
+(add-hook 'mail-mode-hook 'ms-mail)
+(add-hook 'comint-mode-hook 'ms-comint)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
-(add-hook 'ibuffer-mode-hook
+(add-hook 'ibuffer-mode-hook 'ms-ibuffer)
+(add-hook 'eshell-mode-hook 'ms-eshell)
+(add-hook 'emacs-lisp-mode-hook
           (lambda ()
-            (ibuffer-auto-mode 1)
-            (ibuffer-switch-to-saved-filter-groups "default")))
-(add-hook 'eshell-mode-hook
+            (eldoc-mode 1)))
+(add-hook 'js-mode-hook
           (lambda ()
-            (setq eshell-prompt-function (lambda () "$ "))
-            (defun eshell/clear ()
-              "Clear buffer"
-              (interactive)
-              (let ((inhibit-read-only t)) (erase-buffer)))))
+            (setq js-indent-level 2
+                  tab-width 2
+                  c-basic-offset 2)))
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (setq c-basic-offset 4
+                  tab-width 4)))
+(add-hook 'css-mode-hook
+          (lambda ()
+            (setq css-indent-offset 2)))
 
 (set-my-keybindings) ; Keybindings
 (set-my-aliases)     ; Aliases
 (load-my-load-files) ; Load custom elisp files
+
+;; Customizations
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((((type graphic)) (:background "white smoke" :foreground "black")) (t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
+ '(cursor ((((type graphic)) (:background "black" :foreground "white")) (t (:background "white"))))
+ '(highlight ((((class color) (min-colors 88) (background light)) (:background "darkseagreen2")) (((class color) (min-colors 88) (background dark)) (:background "gray14")) (((class color) (min-colors 16) (background light)) (:background "darkseagreen2")) (((class color) (min-colors 16) (background dark)) (:background "gray14")) (((class color) (min-colors 8)) (:background "blue" :foreground "white")) (t (:inverse-video t))))
+ '(magit-item-highlight ((t nil)))
+ '(notmuch-search-count ((((class color) (background dark)) (:foreground "brightcyan")) (((class color) (background light)) (:foreground "cyan4"))))
+ '(notmuch-search-date ((((class color) (background dark)) (:foreground "brightmagenta")) (((class color) (background light)) (:foreground "purple3"))))
+ '(notmuch-search-matching-authors ((((class color) (background dark)) (:foreground "brightyellow")) (((class color) (background light)) (:foreground "red4"))))
+ '(notmuch-tag-face ((((class color) (background dark)) (:foreground "brightgreen")) (((class color) (background light)) (:foreground "blue")) (t (:bold t))))
+ '(rst-level-1-face ((t (:background "grey85" :foreground "black"))) t)
+ '(rst-level-2-face ((t (:background "grey78" :foreground "black"))) t)
+ '(rst-level-3-face ((t (:background "grey71" :foreground "black"))) t)
+ '(rst-level-4-face ((t (:background "grey64" :foreground "black"))) t)
+ '(rst-level-5-face ((t (:background "grey57" :foreground "black"))) t)
+ '(rst-level-6-face ((t (:background "grey50" :foreground "black"))) t)
+ '(widget-field ((((background light)) (:background "light gray" :foreground "black")) (((background dark)) (:background "#3a3a3a" :foreground "#d7ff5f")))))
 
 ;; Encoding
 (prefer-coding-system 'utf-8)
--- emacs.d/pkg-management.el
+++ emacs.d/pkg-management.el
@@ -41,23 +41,33 @@
      (mapc 'ask-to-install (cdr pkglist)))
    my-pkgs-alist))
 
-;; After init function
-(defun pkg-after-init ()
-  ;; auto-complete
-  (when (require 'auto-complete-config nil 'noerror)
-    (ac-config-default))
+;; Autoloads
+(autoload 'ghc-init "ghc" "GHC completion." t)
 
-  ;; Keybindings
-  (when (fboundp 'er/expand-region)
-    (global-set-key (kbd "M-M") 'er/expand-region))
-  (when (fboundp 'win-switch-dispatch)
-    (global-set-key (kbd "C-x o") 'win-switch-dispatch))
+;; Automode
+(setq auto-mode-alist
+      (append
+       '(("\\.markdown$" . markdown-mode)
+         ("\\.md$" . markdown-mode)
+         ("\\.mdown$" . markdown-mode)
+         ("\\.text$" . markdown-mode)
+         ("\\.js$" . js2-mode)
+         ("\\.\\(e\\|h\\)rl$" . erlang-mode)
+         ("\\.clj$" . clojure-mode))
+       auto-mode-alist))
 
-  ;; Aliases
-  (defalias 'git-st 'magit-status)
-  (defalias 'hg-st 'monky-status))
+;; Functions and commands
+(defun jedi ()
+  (interactive)
+  (if (fboundp 'jedi-mode)
+      (jedi-mode (if jedi-mode -1 1))
+    (jedi:setup)
+    (define-key jedi-mode-map (kbd "M-TAB") 'jedi:complete)
+    (define-key jedi-mode-map (kbd "C-c C-d") 'jedi:show-doc)
+    (define-key jedi-mode-map (kbd "C-c C-g") 'jedi:goto-definition)
+    (define-key jedi-mode-map (kbd "C-c r") 'jedi:related-names)))
 
-;; auto-complete
+;; Eval after load
 (eval-after-load "auto-complete"
   '(progn
      (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
@@ -65,7 +75,6 @@
      (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
      (ac-set-trigger-key "TAB")))
 
-;; win-switch
 (eval-after-load "win-switch"
   '(progn
      (setq win-switch-idle-time 1)
@@ -81,5 +90,113 @@
      (win-switch-add-key "K" 'enlarge-vertically)
      (win-switch-add-key "i" 'split-horizontally)))
 
+;; Hook functions
+(defun pkg-after-init ()
+  ;; auto-complete
+  (when (require 'auto-complete-config nil 'noerror)
+    (ac-config-default))
+
+  ;; Keybindings
+  (when (fboundp 'er/expand-region)
+    (global-set-key (kbd "M-M") 'er/expand-region))
+  (when (fboundp 'win-switch-dispatch)
+    (global-set-key (kbd "C-x o") 'win-switch-dispatch))
+
+  ;; Aliases
+  (defalias 'git-st 'magit-status)
+  (defalias 'hg-st 'monky-status))
+
+(defun ms-js2 ()
+  "JavaScript (JS2) hook function."
+  (setq tab-width 2
+        c-basic-offset 2
+        jshint-configuration-path (concat (getenv "HOME") "/.jshint.json")
+        js2-consistent-level-indent-inner-bracket-p t
+        js2-pretty-multiline-decl-indentation-p t
+        js2-basic-offset 2
+        js2-strict-inconsistent-return-warning nil
+        inferior-js-program-command "node")
+  (flymake-mode 1)
+  (local-set-key (kbd "C-x C-e") 'js-send-last-sexp)
+  (local-set-key (kbd "C-M-x") 'js-send-last-sexp-and-go)
+  (local-set-key (kbd "C-c b") 'js-send-buffer)
+  (local-set-key (kbd "C-c b") 'js-send-buffer-and-go)
+  (local-set-key (kbd "C-c l") 'js-load-file-and-go))
+
+(defun ms-magit ()
+  "Magit hook function."
+  (setq fill-column 72)
+  (turn-on-auto-fill))
+
+(defun ms-markdown ()
+  "Markdown hook function."
+  (turn-on-auto-fill)
+  (setq tab-width 4
+        c-basic-offset 4
+        fill-column 79
+        whitespace-line-column 79))
+
+(defun ms-haskell ()
+  "Haskell hook function."
+  (setq tab-width 2
+        haskell-indent-offset 2
+        c-basic-offset 2)
+  (define-key haskell-mode-map (kbd "C-c =") 'haskell-indent-insert-equal)
+  (define-key haskell-mode-map (kbd "C-c |") 'haskell-indent-insert-guard)
+  (define-key haskell-mode-map (kbd "C-c .") 'haskell-mode-format-imports)
+  (turn-on-haskell-indentation)
+  (turn-on-haskell-doc-mode)
+  (when (fboundp 'ghc-init) (ghc-init)))
+
+(defun ms-haskell-ghci ()
+  "Haskell GHCI hook function."
+  (when (require 'ghci-completion nil t)
+    (turn-on-ghci-completion)
+    (when (boundp 'ghc-merged-keyword)
+      (setq pcomplete-command-completion-function
+            (lambda () (pcomplete-here* ghc-merged-keyword))))))
+
+(defun ms-coffee ()
+  "CoffeeScript hook function."
+  (make-local-variable 'tab-width)
+  (setenv "NODE_NO_READLINE" "1")
+  (whitespace-mode 1)
+  (setq coffee-tab-width 2
+        tab-width 2)
+  (define-key coffee-mode-map (kbd "C-c C-r") 'coffee-compile-buffer)
+  (define-key coffee-mode-map (kbd "C-j") 'coffee-newline-and-indent)
+  (define-key coffee-mode-map (kbd "C-m") 'newline))
+
+(defun ms-tex ()
+  "TeX hook function."
+  (setq TeX-auto-save t
+        TeX-PDF-mode t
+        TeX-parse-self t)
+  (add-to-list 'TeX-command-list
+               '("Biber" "biber %s.bcf" TeX-run-BibTeX nil t)))
+
+(defun ms-js-comint ()
+  "JS comint hook function."
+  (ansi-color-for-comint-mode-on)
+  (add-to-list
+   'comint-preoutput-filter-functions
+   (lambda (output)
+     (replace-regexp-in-string
+      ".*1G\.\.\..*5G" "..."
+      (replace-regexp-in-string ".*1G.*3G" "> " output)))))
+
 ;; Hooks
 (add-hook 'after-init-hook 'pkg-after-init)
+(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+(add-hook 'js2-mode-hook 'ms-js2)
+(add-hook 'magit-log-edit-mode-hook 'ms-magit)
+(add-hook 'markdown-mode-hook 'ms-markdown)
+(add-hook 'haskell-mode-hook 'ms-haskell)
+(add-hook 'inferior-haskell-mode-hook 'ms-haskell-ghci)
+(add-hook 'coffee-mode-hook 'ms-coffee)
+(add-hook 'TeX-mode-hook 'ms-tex)
+(setq inferior-js-mode-hook 'ms-js-comint)
+
+(add-hook 'lua-mode-hook
+          (lambda ()
+            (setq lua-indent-level 4)))

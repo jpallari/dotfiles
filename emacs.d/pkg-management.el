a56435cf0ca1e8fe899b9310f5eddb0bcd4df490
Jaakko Pallari <jkpl@lepo.io>
2017-02-06 23:07:36 +0000

    Emacs: smartparens key mappings

--- emacs.d/pkg-management.el
+++ emacs.d/pkg-management.el
@@ -36,6 +36,29 @@
   (win-switch-add-key "i" 'split-horizontally))
 
 (use-package smartparens
+  :bind (:map smartparens-mode-map
+              ("C-M-f" . sp-forward-sexp)
+              ("C-M-b" . sp-backward-sexp)
+              ("C-M-d" . sp-down-sexp)
+              ("C-M-a" . sp-backward-down-sexp)
+              ("C-S-d" . sp-beginning-of-sexp)
+              ("C-S-a" . sp-end-of-sexp)
+              ("C-M-e" . sp-up-sexp)
+              ("C-M-u" . sp-backward-up-sexp)
+              ("C-M-t" . sp-transpose-sexp)
+              ("C-M-n" . sp-next-sexp)
+              ("C-M-p" . sp-previous-sexp)
+              ("C-M-k" . sp-kill-sexp)
+              ("C-M-w" . sp-copy-sexp)
+              ("M-<delete>" . sp-unwrap-sexp)
+              ("M-<backspace>" . sp-backward-unwrap-sexp)
+              ("C-<right>" . sp-forward-slurp-sexp)
+              ("C-<left>" . sp-forward-barf-sexp)
+              ("C-M-<left>" . sp-backward-slurp-sexp)
+              ("C-M-<right>" . sp-backward-barf-sexp)
+              ("M-D" . sp-splice-sexp)
+              ("M-F" . sp-forward-symbol)
+              ("M-B" . sp-backward-symbol))
   :config
   (require 'smartparens-config)
   (add-to-hooks

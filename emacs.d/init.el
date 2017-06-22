4c09cdc99eb2075398650ee7391b55c74b6868a9
Jaakko Pallari <jkpl@lepo.io>
2017-06-22 23:10:30 +0100

    Emacs: merge pkg-management.el to init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,13 +1,8 @@
 ;;; Load paths
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
 (add-to-list 'load-path "~/.emacs.d/vendor")
-(add-to-list 'load-path "~/.emacs.d/elisp")
 (setq custom-theme-directory "~/.emacs.d/themes")
 (when (file-accessible-directory-p "~/.emacs.d/vendor")
   (let ((default-directory "~/.emacs.d/vendor"))
@@ -89,6 +84,11 @@ and writes them to the loaddefs.el file of DIRECTORY"
   (interactive)
   (let ((default-directory (concat org-directory "/")))
     (call-interactively 'ido-find-file)))
+
+;;; Repos
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://stable.melpa.org/packages/")))
 
 ;;; Bindings
 (global-set-key (kbd "C-x ,") 'recompile)
@@ -374,10 +374,163 @@ Ask user a \"y or n\" question only when server has been started."
   ;; Right option as alt gr
   (setq mac-right-option-modifier nil))
 
+;;; use-package
+(if (require 'use-package nil 'noerror)
+    (progn
+      (require 'diminish nil 'noerror)
+      (require 'bind-key nil 'noerror))
+  (progn
+    (message "use-package not found")
+    ;; If use-package is not installed, just create a dummy macro to replace it.
+    (defmacro use-package (&rest args) nil)))
+
+(use-package expand-region
+  :bind ("M-M" . er/expand-region))
+
+(use-package smex
+  :config
+  (global-set-key (kbd "M-x") 'smex)
+  (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+  (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command))
+
+(use-package win-switch
+  :bind ("C-x o" . win-switch-dispatch)
+  :config
+  (setq win-switch-idle-time 1)
+  (win-switch-delete-key "i" 'up)
+  (win-switch-delete-key "I" 'enlarge-vertically)
+  (win-switch-delete-key "o" 'next-window)
+  (win-switch-add-key "n" 'next-window)
+  (win-switch-add-key "h" 'left)
+  (win-switch-add-key "j" 'down)
+  (win-switch-add-key "k" 'up)
+  (win-switch-add-key "H" 'shrink-horizontally)
+  (win-switch-add-key "J" 'shrink-vertically)
+  (win-switch-add-key "K" 'enlarge-vertically)
+  (win-switch-add-key "i" 'split-horizontally))
+
+(use-package paredit
+  :diminish paredit-mode
+  :init
+  (add-hook 'clojure-mode-hook #'enable-paredit-mode)
+  (add-hook 'cider-repl-mode-hook #'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook #'enable-paredit-mode)
+  (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+  (add-hook 'ielm-mode-hook #'enable-paredit-mode))
+
+(use-package iedit
+  :bind ("M-N" . iedit-mode))
+
+(use-package auto-complete
+  :diminish auto-complete-mode
+  :config
+  (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+  (ac-config-default)
+  (setq ac-auto-start nil
+        ac-auto-show-menu nil
+        ac-ignore-case t)
+  (ac-set-trigger-key "TAB"))
+
+(use-package magit
+  :defer t
+  :init
+  (defalias 'git-st 'magit-status))
+
+(use-package markdown-mode
+  :mode
+  "\\.markdown\\'" "\\.md\\'" "\\.text\\'")
+
+(use-package clojure-mode
+  :mode "\\.clj\\'"
+  :init
+  (use-package cider
+    :commands cider-minor-mode
+    :init
+    (add-hook 'cider-mode-hook #'eldoc-mode))
+  :config
+  (add-hook 'clojure-mode-hook #'eldoc-mode))
+
+(use-package go-mode
+  :mode "\\.go\\'"
+  :init
+  (add-hook 'before-save-hook #'gofmt-before-save)
+  :config
+  (add-hook 'go-mode-hook #'electric-pair-local-mode)
+  (add-hook 'go-mode-hook #'subword-mode))
+
+(use-package go-eldoc
+  :init
+  (add-hook 'go-mode-hook 'go-eldoc-setup))
+
+(use-package haskell-mode
+  :mode "\\.hs\\'"
+  :bind (:map haskell-mode-map
+              ("C-c ." . haskell-mode-format-imports)
+              ("C-j"   . haskell-newline-and-indent)
+              ("C-m"   . newline))
+  :config
+  (setq-default haskell-indent-offset 4
+                haskell-indentation-layout-offset 4
+                haskell-indentation-left-offset 4
+                haskell-indentation-ifte-offset 4)
+  (add-hook 'haskell-mode-hook #'turn-on-haskell-indendation)
+  (add-hook 'haskell-mode-hook #'turn-on-haskell-doc)
+  (add-hook 'haskell-mode-hook #'electric-pair-local-mode)
+  (add-hook 'haskell-mode-hook #'subword-mode))
+
+(use-package AucTex
+  :defer t
+  :config
+  (setq-default TeX-auto-save t
+                TeX-PDF-mode t
+                TeX-parse-self t
+                LaTeX-verbatim-environments '("Verbatim" "lstlisting"))
+  (add-to-list 'TeX-command-list
+               '("Biber" "biber %s.bcf" TeX-run-BibTeX nil t)))
+
+(use-package js2-mode
+  :mode "\\.js\\'"
+  :bind (:map js2-mode-map
+              ("C-x C-e" . js-send-last-sexp)
+              ("C-M-x"   . js-send-last-sexp-and-go)
+              ("C-c b"   . js-send-buffer)
+              ("C-c C-b" . js-send-buffer-and-go)
+              ("C-c g"   . js-send-region)
+              ("C-c C-g" . js-send-region-and-go)
+              ("C-c l"   . js-load-file-and-go))
+  :config
+  (setq-default js2-basic-offset 2
+                js2-strict-inconsistent-return-warning nil
+                inferior-js-program-command "node")
+  (setq-default
+   inferior-js-mode-hook
+   (lambda ()
+     (ansi-color-for-comint-mode-filter)
+     (subword-mode)
+     (add-to-list
+      'comint-preoutput-filter-functions
+      (lambda (output)
+        (replace-regexp-in-string "\e\\[[0-9]+[GKJ]" "" output)))
+     (setq comint-process-echoes t)))
+  (add-hook 'js2-mode-hook #'electric-pair-local-mode)
+  (add-hook 'js2-mode-hook #'subword-mode))
+
+(use-package scala-mode
+  :mode ("\\.scala\\'" . scala-mode)
+  :interpreter ("scala" . scala-mode)
+  :config
+  (add-hook 'scala-mode-hook #'electric-pair-local-mode)
+  (add-hook 'scala-mode-hook #'subword-mode))
+
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns))
+  :config
+  (exec-path-from-shell-initialize))
+
 ;;; Load my stuff
 (mapc (lambda (filename) (load filename t t t))
-      '("~/.emacs.d/pkg-management.el"
-        "~/.emacs.d/vendor/loaddefs.el"
+      '("~/.emacs.d/vendor/loaddefs.el"
         "~/.emacs.local"))
 
 ;;; Customizations

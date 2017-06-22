a4bf6be937a3e4fb749e81825b3b09fe5a73b60a
Jaakko Pallari <jkpl@lepo.io>
2017-06-22 23:01:16 +0100

    Emacs: Clean up selected packages

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -388,7 +388,7 @@ Ask user a \"y or n\" question only when server has been started."
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (cl-lib let-alist spinner smex elm-mode ox-pandoc htmlize yaml-mode win-switch utop use-package scala-mode2 scala-mode sbt-mode rust-mode paredit markdown-mode magit less-css-mode js2-mode js-comint iedit groovy-mode gradle-mode go-mode ghc fuzzy flymake-python-pyflakes expand-region exec-path-from-shell erlang auctex ag ac-cider clojure-mode cider popup go-rename go-eldoc go-autocomplete auto-complete))))
+    (smex elm-mode ox-pandoc htmlize yaml-mode win-switch utop use-package scala-mode2 scala-mode sbt-mode rust-mode paredit markdown-mode magit less-css-mode js2-mode js-comint iedit groovy-mode gradle-mode go-mode ghc fuzzy expand-region exec-path-from-shell auctex ag ac-cider clojure-mode cider go-rename go-eldoc go-autocomplete auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

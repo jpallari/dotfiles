97312c4c32261aee3ab8a8f97454f53b3a1431bf
Jaakko Pallari <jp@lepovirta.org>
2024-07-11 15:52:10 +0300

    wezterm: color scheme, key bindings
    - Color scheme switcher
    - Disable ligatures
    - URL quick selector
    - Tab navigator

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,8 +5,26 @@ local config = wezterm.config_builder()
 config.automatically_reload_config = false
 
 -- Appearance
-config.color_scheme = 'tokyonight'
+config.color_scheme = 'duskfox'
 config.font = wezterm.font 'JetBrainsMono NF'
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- disable ligatures
+local colorscheme_switches = {
+  dayfox = 'carbonfox',
+  carbonfox = 'nightfox'
+}
+
+-- Events
+wezterm.on('toggle-colorscheme', function(window)
+  local overrides = window:get_config_overrides() or {}
+  if not overrides.color_scheme then
+    overrides.color_scheme = 'dayfox'
+  else
+    local next_color_scheme = colorscheme_switches[overrides.color_scheme]
+    overrides.color_scheme = next_color_scheme
+  end
+
+  window:set_config_overrides(overrides)
+end)
 
 -- Key bindings
 config.use_dead_keys = false
@@ -36,6 +54,28 @@ config.keys = {
     action = wezterm.action.PaneSelect {
       mode = 'SwapWithActive',
     },
+  },
+  {
+    key = 'm',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.QuickSelectArgs {
+      label = 'open url',
+      patterns = { 'https?://\\S+' },
+      action = wezterm.action_callback(function(window, pane)
+        local url = window:get_selection_text_for_pane(pane)
+        wezterm.open_with(url)
+      end),
+    },
+  },
+  {
+    key = 'i',
+    mods = 'CTRL|SHIFT|ALT',
+    action = wezterm.action.EmitEvent 'toggle-colorscheme',
+  },
+  {
+    key = '.',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ShowTabNavigator,
   },
 }
 

0a27022d85154310319b9f54e80147d5e4bcaf9f
Jaakko Pallari <jp@lepovirta.org>
2024-11-28 21:35:37 +0200

    neovim: git branch out of status line

--- nvim/init.lua
+++ nvim/init.lua
@@ -912,11 +912,33 @@ require('lazy').setup({
       }
 
       local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      local function statusline_sections()
+        local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+        local diff          = statusline.section_diff({ trunc_width = 75 })
+        local diagnostics   = statusline.section_diagnostics({ trunc_width = 75 })
+        local lsp           = statusline.section_lsp({ trunc_width = 75 })
+        local filename      = statusline.section_filename({ trunc_width = 140 })
+        local fileinfo      = statusline.section_fileinfo({ trunc_width = 120 })
+        local location      = statusline.section_location({ trunc_width = 75 })
+        local search        = statusline.section_searchcount({ trunc_width = 75 })
 
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
+        return statusline.combine_groups({
+          { hl = mode_hl,                  strings = { mode } },
+          { hl = 'MiniStatuslineDevinfo',  strings = { diff, diagnostics, lsp } },
+          '%<', -- Mark general truncate point
+          { hl = 'MiniStatuslineFilename', strings = { filename } },
+          '%=', -- End left alignment
+          { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+          { hl = mode_hl,                  strings = { search, location } },
+        })
+      end
+      statusline.setup {
+        content = {
+          active = statusline_sections,
+          inactive = statusline_sections,
+        },
+        use_icons = vim.g.have_nerd_font,
+      }
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
         return '%2l:%-2v'

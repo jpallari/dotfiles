70a2972881d3117ff0b87d3a0b2918e8d5344eef
Jaakko Pallari <jp@lepovirta.org>
2025-01-10 21:51:10 +0200

    neovim: update plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -735,6 +735,8 @@ require('lazy').setup({
 
       local lspconfig = require('lspconfig')
       require('mason-lspconfig').setup {
+        ensure_installed = {},
+        automatic_installation = false,
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}

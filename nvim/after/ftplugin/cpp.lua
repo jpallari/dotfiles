811402a6c4f0308e036acb4baa26988abb5a26d1
Jaakko Pallari <jp@lepovirta.org>
2025-03-01 13:44:31 +0200

    neovim: c/c++ indentation

--- nvim/after/ftplugin/cpp.lua
+++ nvim/after/ftplugin/cpp.lua
@@ -2,4 +2,4 @@ vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.cindent = true
-vim.opt_local.cinoptions = ':0g0'
+vim.opt_local.cinoptions = ':0g0E-s(0w1W4m1'

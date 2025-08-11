f5ac624efcedd27c2e37f2153596324c4c15f8d7
Jaakko Pallari <jp@lepovirta.org>
2025-08-11 10:55:20 +0300

    wezterm: clean

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,7 @@ config.automatically_reload_config = false
 
 -- Appearance
 config.window_decorations = 'RESIZE|INTEGRATED_BUTTONS'
-config.color_scheme = 'iTerm2 Default'
+config.color_scheme = 'Builtin Dark'
 config.font = wezterm.font_with_fallback {
   'AcPlus IBM VGA 9x16',
   'JetBrainsMono NF',
@@ -18,24 +18,6 @@ config.line_height = 0.9
 config.freetype_load_flags = 'NO_HINTING'
 config.bold_brightens_ansi_colors = 'BrightOnly'
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- disable ligatures
-local colorscheme_switches = {
-  dayfox = 'carbonfox',
-  carbonfox = 'nightfox',
-  nightfox = 'duskfox',
-}
-
--- Events
-wezterm.on('toggle-colorscheme', function(window)
-  local overrides = window:get_config_overrides() or {}
-  if overrides.color_scheme then
-    local next_color_scheme = colorscheme_switches[overrides.color_scheme]
-    overrides.color_scheme = next_color_scheme
-  else
-    overrides.color_scheme = 'dayfox'
-  end
-
-  window:set_config_overrides(overrides)
-end)
 
 -- Key bindings
 config.use_dead_keys = false
@@ -77,11 +59,6 @@ config.keys = {
         wezterm.open_with(url)
       end),
     },
-  },
-  {
-    key = 'i',
-    mods = 'CTRL|SHIFT|ALT',
-    action = wezterm.action.EmitEvent 'toggle-colorscheme',
   },
   {
     key = '.',
